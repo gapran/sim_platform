d439566b929872220b83a5f6f6cc92475d15d69b
Lisa Nguyen <lisa.nqd@gmail.com>
2019-02-05 18:29:34 +0100

    Added application example

---
+++ SimPlatform/main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+#include "mainwindow.h"
+
+/*
+ * Example from:
+ * http://doc.qt.io/qt-5/qtwidgets-mainwindows-application-example.html
+ */
+
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(application);
+
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationName("Application Example");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    MainWindow mainWin;
+    if (!parser.positionalArguments().isEmpty())
+        mainWin.loadFile(parser.positionalArguments().first());
+    mainWin.show();
+    return app.exec();
+}
