5b2632cdacd8080d2732727452f7ab004f427c37
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-03-06 18:56:06 +0100

    Creates main container

--- SimPlatform/main.cpp
+++ SimPlatform/main.cpp
@@ -1,10 +1,25 @@
 #include "gpalib.h"
 
-int main()
+#include <QApplication>
+#include <QWidget>
+#include <QtGui>
+
+int main(int argc, char *argv[])
 {
 
     // Test the library linked
     GPAlib gpaLib;
     gpaLib.Test();
+
+    QApplication app(argc, argv);
+
+    //Create Main Container
+    QWidget window;
+        window.resize(900, 300);
+        window.show();
+        window.setWindowTitle(
+            QApplication::translate("toplevel", "GPA Prototype"));
+
+    return app.exec();
 
 }
