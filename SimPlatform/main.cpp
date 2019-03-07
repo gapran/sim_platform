8c37fddd5afcb306b455930fc546388fc8f61f5c
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-03-07 08:27:25 +0100

    Creates Child Widget inside Main Container

--- SimPlatform/main.cpp
+++ SimPlatform/main.cpp
@@ -1,6 +1,7 @@
 #include "gpalib.h"
 
 #include <QApplication>
+#include <QPushButton>
 #include <QWidget>
 #include <QtGui>
 
@@ -19,6 +20,12 @@ int main(int argc, char *argv[])
         window.show();
         window.setWindowTitle(
             QApplication::translate("toplevel", "GPA Prototype"));
+
+        //Create Child Widget
+        QPushButton *button = new QPushButton(
+                QApplication::translate("toplevel", "Press me"), &window);
+            button->move(100, 100);
+            button->show();
 
     return app.exec();
 
