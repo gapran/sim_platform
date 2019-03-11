ee052b686b299e34d9ed7c0450403c8b8b71efcd
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-03-11 19:13:05 +0100

    Removes Layout implementation and Cleans Code

--- Prototype01/main.cpp
+++ Prototype01/main.cpp
@@ -27,31 +27,9 @@ int main(int argc, char *argv[])
 
         //Create inner containers
 
-        QWidget* container1 = container.createContainer( 50, 50, 10, 10, "Container 1", "blue");
-        QWidget* container2 = container.createContainer( 50, 50, 80, 10, "Container 2", "orange");
-        QWidget* container3 = container.createContainer( 50, 50, 150, 10, "Container 3", "brown");
-
-
-
-        //Specify Layout for the containers
-
-        Container layoutContainer1;
-        Container layoutContainer2;
-
-        QWidgetList containerList1;
-        containerList1.append(container1);
-        containerList1.append(container2);
-
-
-       layoutContainer1.createLayout(&mainContainerView, "horizontalLayout", containerList1);
-
-
-       // Add 3rd container in vertical layout
-
-//       QWidgetList containerList2;
-//       containerList1.append(container3);
-//       layoutContainer2.createLayout(&mainContainerView, "verticalLayout", containerList2);
-
+     container.createContainer( 50, 50, 10, 10, "Container 1", "blue");
+     container.createContainer( 50, 50, 80, 10, "Container 2", "orange");
+     container.createContainer( 50, 50, 150, 10, "Container 3", "brown");
 
 
    //User Code ends
--- SimPlatform/container.h
+++ SimPlatform/container.h
@@ -1,7 +1,6 @@
 #ifndef CONTAINER_H
 #define CONTAINER_H
 
-#include <QBoxLayout>
 #include <QtWidgets>
 #include <iostream>
 #include <QIcon>
@@ -17,18 +16,17 @@ private:
 
 public:
 
-    QMainWindow* createMainContainer( QMainWindow *mainWindow ,int length, int width, QString title, QString colour){
+    void createMainContainer( QMainWindow *mainWindow ,int length, int width, QString title, QString colour){
 
     mainWindow->setWindowTitle(title);
     mainWindow->resize(length,width);
     mainWindow->setStyleSheet("background-color: "+colour+";");
     mainWindow->setWindowIcon(QIcon(":/resources/icon/gpa_logo.png"));
+    mainWindow->show();
 
-    return mainWindow;
+}
 
-    }
-
-    QWidget* createContainer(int length, int width, int positionX, int positionY, QString title, QString colour){
+   void createContainer(int length, int width, int positionX, int positionY, QString title, QString colour){
 
         QWidget* qWidget = new QWidget();
 
@@ -36,57 +34,9 @@ public:
             qWidget->move(positionX, positionY);
             qWidget->setToolTip(title);
             qWidget->setStyleSheet("background-color: "+colour+";");
-            return qWidget;
-
+            qWidget->show();
     }
 
-    /*
-     * Method parameters:
-     * 1. Provides view of Main Container
-     * 2. Defines what kind of layout is required
-     * 3. List of containers
-     */
- void createLayout(QMainWindow *mainContainerView, QString layoutName, QWidgetList containerList){
-
-        QWidget *centralWidget = new QWidget (mainContainerView);
-        QVBoxLayout *widgetLayout = new QVBoxLayout(centralWidget);
-        QHBoxLayout *mainLayout = new QHBoxLayout;
-        QHBoxLayout *horizontalLayout1 = new QHBoxLayout;
-        QVBoxLayout *verticalLayout1 = new QVBoxLayout;
-
-
-        if( layoutName == "horizontalLayout"){
-
-            for (int var = 0; var < containerList.size(); ++var) {
-                horizontalLayout1->addWidget(containerList.at(var));
-            }
-
-            mainLayout->addLayout(horizontalLayout1);
-
-            widgetLayout->addLayout(mainLayout);
-            mainContainerView->setCentralWidget(centralWidget);
-
-
-        } else if (layoutName == "verticalLayout") {
-
-            for (int var = 0; var < containerList.size(); ++var) {
-                verticalLayout1->addWidget(containerList.at(var));
-            }
-
-           mainLayout->addLayout(verticalLayout1);
-
-           widgetLayout->addLayout(mainLayout);
-           mainContainerView->setCentralWidget(centralWidget);
-
-
-        } else {
-
-            clog << "Please provide valid input." << endl;
-
-        }
-
-
-}
 };
 
 #endif // CONTAINER_H
