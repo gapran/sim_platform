baf58c10e6feee55701d58c1c7d7217386d6ab8d
osamic <osamaalic@gmail.com>
2019-03-11 01:04:26 +0100

    changing script testing

--- SimPlatform/gpacomponent.h
+++ SimPlatform/gpacomponent.h
@@ -11,7 +11,6 @@ class GpaComponent
 public:
     enum Colours{NORMAL,RED,GREEN ,YELLOW,BLUE,MAGENTA,CYAN,WHITE,RESET};
     GpaComponent();
-    void main();
     void mainView(QMainWindow *mW , QString s ,int l , int w , QString c);
     QWidget* newComponent(int w, int h, int posx, int posy, QBoxLayout* ql, QString color, QString ttp);
     QVBoxLayout* newVLayout();
