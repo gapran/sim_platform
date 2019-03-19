fa0ed7eab809fc48738934416a5e87d7170ebbc9
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-03-19 10:51:33 +0100

    Displays Text in a QWidget

--- SimPlatform/main.cpp
+++ SimPlatform/main.cpp
@@ -1,6 +1,26 @@
 #include "main.h"
+#include "mainwindow.h"
+#include "text.h"
 
-int main()
+/*
+ * Example from:
+ * http://doc.qt.io/qt-5/qtwidgets-mainwindows-application-example.html
+ */
+
+int main(int argc, char *argv[])
+
 {
 
+    //Text Element
+    QWidget *textWidget = new QWidget();
+    Text *text = new Text(textWidget);
+    text->setText("Welcome to GPA");
+    text->show();
+    textWidget->show();
+
+    MainWindow mainWin;
+    if (!parser.positionalArguments().isEmpty())
+        mainWin.loadFile(parser.positionalArguments().first());
+    mainWin.show();
+    return app.exec();
 }
