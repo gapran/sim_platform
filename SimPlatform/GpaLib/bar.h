007f7f72190ce61ab8fd5e28ed015d17bcc70f87
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-04-24 11:35:53 +0200

    Disables update when required

--- SimPlatform/GpaLib/bar.h
+++ SimPlatform/GpaLib/bar.h
@@ -2,25 +2,24 @@
 #define IMAGE_COPY_H
 
 #include <QDialog>
-#include<QProgressBar>
 #include <QHBoxLayout>
+#include <QProgressBar>
 
 using namespace std;
 class Qlabel;
 
-class Bar: public QDialog
-{
+class Bar : public QDialog {
     Q_OBJECT
 public:
     explicit Bar(QWidget *parent = 0, int min = 0, int max = 100);
 
-    void setValue(int current) {
-        bar->setValue(current);
-    }
+    void setValue(int current) { bar->setValue(current); }
     void setText(QString text) { bar->setToolTip(text); }
     void updatable(bool value) {
-        if(value == true) {
+        if (value == true) {
             bar->updatesEnabled();
+        } else {
+            bar->setUpdatesEnabled(false);
         }
     }
 
