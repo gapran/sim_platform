368b37f672e330bd98e4ce5dda0396be45a491b5
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-04-26 11:19:50 +0200

    Fix Nullability bug

--- SimPlatform/GpaLib/bar.h
+++ SimPlatform/GpaLib/bar.h
@@ -11,7 +11,7 @@ class Qlabel;
 class Bar : public QDialog {
     Q_OBJECT
 public:
-    explicit Bar(QWidget *parent = 0, int min = 0, int max = 100);
+    explicit Bar(QWidget *parent = nullptr, int min = 0, int max = 100);
 
     void setValue(int current) { bar->setValue(current); }
     void setText(QString text) { bar->setToolTip(text); }
