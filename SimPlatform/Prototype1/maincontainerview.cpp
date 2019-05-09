61252836e35e040565011e05339eb21414bd1d3e
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-05-09 16:57:25 +0200

    Adds desired layout as background image
    Here layout means - web, cli, ide, mobile.

--- SimPlatform/Prototype1/maincontainerview.cpp
+++ SimPlatform/Prototype1/maincontainerview.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    // Desired layout has be given here as a background image
+    // Layout Examples: cli, ide, mobile, web
+    // Replace the filename with desired layout name
+    this->centralWidget()->setStyleSheet(
+        "border-image:url(:/resources/icon/ide.jpg)0 0 0 0 stretch "
+        "stretch;");
 }
 
 MainWindow::~MainWindow() {
