5bcf82ec856bd7f7161f3c373e957f117f176518
gsvarma <17321286+gsvarma@users.noreply.github.com>
2019-05-10 15:22:55 +0200

    Merges layout image with GUI elements

--- SimPlatform/Prototype1/main.cpp
+++ SimPlatform/Prototype1/main.cpp
@@ -46,33 +46,33 @@ int main(int argc, char *argv[]) {
         "border-image:url(:/resources/icon/ide.jpg)0 0 0 0 stretch "
         "stretch;");
 
-    //    // Layout Management - https://doc.qt.io/qt-5/layout.html
+    // Layout Management - https://doc.qt.io/qt-5/layout.html
 
-    //    QPushButton *button1 = new QPushButton("Button 1");
-    //    QPushButton *button2 = new QPushButton("Button 2");
-    //    QPushButton *button3 = new QPushButton("Button 3");
+    QPushButton *button1 = new QPushButton("Button 1");
+    QPushButton *button2 = new QPushButton("Button 2");
+    QPushButton *button3 = new QPushButton("Button 3");
 
-    //    // Image
-    //    QWidget *image1Widget = new
-    //    QWidget(mainContainerView.centralWidget()); Image *image = new
-    //    Image(image1Widget); QString filename =
-    //    ":/resources/icon/bronze_image.jpg"; image->setImage(filename);
-    //    image->setHover("This is Bronze Image.");
-    //    image->noBorder();
+    // Image
+    QWidget *image1Widget = new QWidget(mainContainerView.centralWidget());
+    Image *image = new Image(image1Widget);
+    QString filename = ":/resources/icon/bronze_image.jpg";
+    image->setImage(filename);
+    image->setHover("This is Bronze Image.");
+    image->noBorder();
 
-    //    // Progress bar
-    //    QWidget *imageCopy1Widget = new
-    //    QWidget(mainContainerView.centralWidget()); Bar *bar = new
-    //    Bar(imageCopy1Widget, 0, 100); bar->setValue(60);
+    // Progress bar
+    QWidget *imageCopy1Widget = new QWidget(mainContainerView.centralWidget());
+    Bar *bar = new Bar(imageCopy1Widget, 0, 100);
+    bar->setValue(60);
 
-    //    QGridLayout *layout = new QGridLayout;
-    //    layout->addWidget(image, 0, 0);
-    //    layout->addWidget(bar, 0, 1);
-    //    layout->addWidget(button1, 1, 0, 1, 2);
-    //    layout->addWidget(button2, 2, 0);
-    //    layout->addWidget(button3, 2, 1);
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(image, 0, 0);
+    layout->addWidget(bar, 0, 1);
+    layout->addWidget(button1, 1, 0, 1, 2);
+    layout->addWidget(button2, 2, 0);
+    layout->addWidget(button3, 2, 1);
 
-    //    mainContainerView.centralWidget()->setLayout(layout);
+    mainContainerView.centralWidget()->setLayout(layout);
 
     // User Code ends
 
